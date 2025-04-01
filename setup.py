9cd7d2c1c0518e6a76267468b140ace9d0a1f9df
James Berthoty <confusedcrib@gmail.com>
2025-04-01 16:11:46 -0400

    fix requirement

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='latio',
-    version='v1.2.0',
+    version='v1.2.2',
     url='https://github.com/latiotech/LAST',
     license='GPL-3.0 license',
     author='James Berthoty',
